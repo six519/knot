4f636f603b3a6d2474d8d1d928a45bf8acd6ae62
Jan Hák <jan.hak@nic.cz>
2021-09-29 14:00:44 +0200

    knotd: Add QUIC transport layer to knotd (WiP)

--- src/knot/server/udp-handler.c
+++ src/knot/server/udp-handler.c
@@ -20,15 +20,23 @@
 #include <dlfcn.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <unistd.h>
+
 #include <arpa/inet.h>
+#include <gnutls/gnutls.h>
+#include <netinet/in.h>
 #include <sys/param.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #ifdef HAVE_SYS_UIO_H	// struct iovec (OpenBSD)
 #include <sys/uio.h>
 #endif /* HAVE_SYS_UIO_H */
-#include <unistd.h>
+
+#ifdef LIBNGTCP2
+#include <ngtcp2/ngtcp2.h>
+#include <ngtcp2/ngtcp2_crypto.h>
+#include <ngtcp2/ngtcp2_crypto_gnutls.h>
+#endif
 
 #include "contrib/macros.h"
 #include "contrib/mempattern.h"
@@ -40,6 +48,8 @@
 #include "knot/server/server.h"
 #include "knot/server/udp-handler.h"
 #include "knot/server/xdp-handler.h"
+
+#define QUIC_SV_SCIDLEN 18
 
 /* Buffer identifiers. */
 enum {
@@ -391,14 +401,345 @@ static udp_api_t xdp_recvmmsg_api = {
 };
 #endif /* ENABLE_XDP */
 
+
 static bool is_xdp_thread(const server_t *server, int thread_id)
 {
-	return server->handlers[IO_XDP].size > 0 &&
-	       server->handlers[IO_XDP].handler.thread_id[0] <= thread_id;
+	const unsigned size = server->handlers[IO_XDP].size;
+	const unsigned *ids = server->handlers[IO_XDP].handler.thread_id;
+	return size > 0 && thread_id >= ids[0] && thread_id <= ids[size - 1];
 }
 
-static int iface_udp_fd(const iface_t *iface, int thread_id, bool xdp_thread,
-                        void **xdp_socket)
+#ifdef LIBNGTCP2
+typedef struct knot_quic_conn {
+	node_t n;
+	ngtcp2_cid dcid;
+	ngtcp2_conn *conn;
+	struct knot_quic_conn *next;
+} knot_quic_conn_t;
+
+typedef struct {
+	size_t size;
+	size_t usage;
+	size_t inbufs_total;
+	knot_quic_conn_t *conns[];
+} knot_quic_table_t;
+
+static knot_quic_table_t *knot_quic_table_new(size_t size)
+{
+	knot_quic_table_t *table = calloc(1, sizeof(*table) + //sizeof(list_t) +
+	                                    size * sizeof(table->conns[0]));
+	if (table == NULL) {
+		return table;
+	}
+
+	table->size = size;
+	//init_list(tcp_table_timeout(table));
+
+	return table;
+}
+
+static uint64_t knot_quic_conn_hash(const ngtcp2_cid *dcid)
+{
+	uint64_t hash = 0;
+	const size_t size = MIN(dcid->datalen, NGTCP2_MAX_CIDLEN); 
+	const uint8_t *end = (uint8_t *)(dcid->data + size);
+	uint64_t *it1 = NULL;
+	for (it1 = dcid->data; it1 < end && (end - (uint8_t *)it1) >= sizeof(uint64_t); ++it1) {
+		hash ^= *it1;
+	}
+	size_t shift = sizeof(uint64_t) - 1;
+	for (uint8_t *it2 = (uint8_t *)it1; it2 < end; ++it2) {
+		hash ^= ((*it2) << (shift-- * 8));
+	}
+
+	return hash;
+}
+
+static knot_quic_conn_t *knot_quic_table_add(knot_quic_table_t *table, const ngtcp2_cid *dcid)
+{
+	knot_quic_conn_t *conn = calloc(1, sizeof(knot_quic_conn_t));
+	if (conn == NULL) {
+		return NULL;
+	}
+	ngtcp2_cid_init(&conn->dcid, dcid->data, dcid->datalen);
+
+	uint64_t hash = knot_quic_conn_hash(&conn->dcid) % table->size;
+	conn->next = table->conns[hash];
+	return (table->conns[hash] = conn);
+}
+
+static knot_quic_conn_t *knot_quic_table_find(knot_quic_table_t *table, const ngtcp2_cid *dcid)
+{
+	uint64_t hash = knot_quic_conn_hash(dcid) % table->size;
+	knot_quic_conn_t *conn = table->conns[hash];
+	while (conn != NULL) {
+		if (ngtcp2_cid_eq(&conn->dcid, dcid) != 0) {
+			break;
+		}
+		conn = conn->next;
+	}
+	return conn;
+}
+
+struct quic_recvfrom {
+	int fd;
+	struct sockaddr_storage addr;
+	struct msghdr msg[NBUFS];
+	struct iovec iov[NBUFS];
+	uint8_t buf[NBUFS][KNOT_WIRE_MAX_PKTSIZE];
+	cmsg_pktinfo_t pktinfo;
+	gnutls_certificate_credentials_t tls_creds;
+	gnutls_anti_replay_t tls_anti_replay;
+	gnutls_datum_t tls_ticket_key;
+	knot_quic_table_t *conns;
+	//gnutls_session_t tls_ctx;
+};
+
+static int secret_func(gnutls_session_t session,
+                       gnutls_record_encryption_level_t gtls_level,
+                       const void *rx_secret, const void *tx_secret,
+                       size_t secretlen) {
+	struct quic_recvfrom *ctx = (struct quic_recvfrom *)gnutls_session_get_ptr(session);
+	int level = ngtcp2_crypto_gnutls_from_gnutls_record_encryption_level(gtls_level);
+	// if (rx_secret &&
+	//     ngtcp2_crypto_derive_and_install_rx_key(ctx->conn, NULL, NULL, NULL,
+	// 	        level, rx_secret, secretlen) != 0) {
+	// 	return -1;
+	// }
+
+	if (tx_secret) {
+		// if (ngtcp2_crypto_derive_and_install_tx_key(ctx->conn, NULL, NULL,
+		//         NULL, level, tx_secret, secretlen) != 0) {
+		// 	return -1;
+		// }
+		// TODO uncomment when `call_application_tx_key_cb != NULL` 
+		// if (level == NGTCP2_CRYPTO_LEVEL_APPLICATION && call_application_tx_key_cb(ctx) != 0) {
+		// 	return -1;
+		// }
+	}
+
+	return 0;
+}
+
+static int anti_replay_db_add_func(void *dbf, time_t exp_time,
+                                   const gnutls_datum_t *key,
+                                   const gnutls_datum_t *data)
+{
+	return 0;
+}
+
+#define QUIC_DEFAULT_VERSION "-VERS-ALL:+VERS-TLS1.3"
+#define QUIC_DEFAULT_CIPHERS "-CIPHER-ALL:+AES-128-GCM:+AES-256-GCM:+CHACHA20-POLY1305:+AES-128-CCM"
+#define QUIC_DEFAULT_GROUPS  "-GROUP-ALL:+GROUP-SECP256R1:+GROUP-X25519:+GROUP-SECP384R1:+GROUP-SECP521R1"
+#define QUIC_PRIORITIES      "%DISABLE_TLS13_COMPAT_MODE:NORMAL:"QUIC_DEFAULT_VERSION":"QUIC_DEFAULT_CIPHERS":"QUIC_DEFAULT_GROUPS
+
+static void session_ticket_key_free(gnutls_datum_t *ticket) {
+	gnutls_memset(ticket->data, 0, ticket->size);
+	gnutls_free(ticket->data);
+}
+
+static void *quic_recvfrom_init(_unused_ void *xdp_sock)
+{
+	struct quic_recvfrom *rq = malloc(sizeof(struct quic_recvfrom));
+	if (rq == NULL) {
+		return NULL;
+	}
+	memset(rq, 0, sizeof(struct quic_recvfrom));
+
+	for (unsigned i = 0; i < NBUFS; ++i) {
+		rq->iov[i].iov_base = rq->buf + i;
+		rq->iov[i].iov_len = KNOT_WIRE_MAX_PKTSIZE;
+		rq->msg[i].msg_name = &rq->addr;
+		rq->msg[i].msg_namelen = sizeof(rq->addr);
+		rq->msg[i].msg_iov = &rq->iov[i];
+		rq->msg[i].msg_iovlen = 1;
+		rq->msg[i].msg_control = &rq->pktinfo.cmsg;
+		rq->msg[i].msg_controllen = sizeof(rq->pktinfo);
+	}
+
+	// TLS certificates
+	int ret = gnutls_certificate_allocate_credentials(&rq->tls_creds);
+	if (ret != GNUTLS_E_SUCCESS) {
+		free(rq);
+		return NULL;
+	}
+
+	ret = gnutls_certificate_set_x509_system_trust(rq->tls_creds);
+	if (ret < 0) {
+		gnutls_certificate_free_credentials(rq->tls_creds);
+		free(rq);
+		return NULL;
+	}
+
+	conf_val_t crt_val = conf_get(conf(), C_SRV, C_QUIC_CERT_FILE);
+	const char *cert_file = conf_str(&crt_val);
+	crt_val = conf_get(conf(), C_SRV, C_QUIC_KEY_FILE);
+	const char *key_file = conf_str(&crt_val);
+	if (cert_file != NULL && key_file != NULL) {
+		ret = gnutls_certificate_set_x509_key_file(rq->tls_creds,
+		        cert_file, key_file, GNUTLS_X509_FMT_PEM);
+		if (ret != GNUTLS_E_SUCCESS) {
+			gnutls_certificate_free_credentials(rq->tls_creds);
+			free(rq);
+			return NULL;
+		}
+	}
+
+	ret = gnutls_session_ticket_key_generate(&rq->tls_ticket_key);
+	if (ret != GNUTLS_E_SUCCESS) {
+		gnutls_certificate_free_credentials(rq->tls_creds);
+		free(rq);
+		return -1;
+	}
+
+	ret = gnutls_anti_replay_init(&rq->tls_anti_replay);
+	if (ret != GNUTLS_E_SUCCESS) {
+		session_ticket_key_free(&rq->tls_ticket_key);
+		gnutls_certificate_free_credentials(rq->tls_creds);
+		free(rq);
+	}
+	gnutls_anti_replay_set_add_function(rq->tls_anti_replay,
+	                                    anti_replay_db_add_func);
+	gnutls_anti_replay_set_ptr(rq->tls_anti_replay, NULL);
+
+	rq->conns = knot_quic_table_new(100); //TODO configurable
+
+	//conf()->cache.srv_quic_secret;
+
+	// TLS session
+	// ret = gnutls_init(&rq->tls_ctx, GNUTLS_SERVER | 
+	//         GNUTLS_ENABLE_EARLY_DATA | GNUTLS_NO_AUTO_SEND_TICKET |
+	//         GNUTLS_NO_END_OF_EARLY_DATA);
+	// if (ret != 0) {
+	// 	gnutls_anti_replay_deinit(rq->tls_anti_replay);
+	// 	session_ticket_key_free(&rq->tls_ticket_key);
+	// 	gnutls_certificate_free_credentials(rq->tls_creds);
+	// 	free(rq);
+	// 	return NULL;
+	// }
+
+	// ret = gnutls_priority_set_direct(rq->tls_ctx, QUIC_PRIORITIES, NULL);
+	// if (ret != 0) {
+	// 	gnutls_deinit(rq->tls_ctx);
+	// 	gnutls_anti_replay_deinit(rq->tls_anti_replay);
+	// 	session_ticket_key_free(&rq->tls_ticket_key);
+	// 	gnutls_certificate_free_credentials(rq->tls_creds);
+	// 	free(rq);
+	// 	return NULL;
+	// }
+
+	// gnutls_datum_t ticket_key;	
+	// ret = gnutls_session_ticket_key_generate(&ticket_key);
+	// if (ret != 0) {
+	// 	gnutls_deinit(rq->tls_ctx);
+	// 	gnutls_certificate_free_credentials(rq->tls_creds);
+	// 	free(rq);
+	// 	return NULL;
+	// }
+
+	// ret = gnutls_session_ticket_enable_server(rq->tls_ctx, &ticket_key);
+	// if (ret != 0) {
+	// 	gnutls_memset(ticket_key.data, 0, ticket_key.size);
+	// 	gnutls_free(ticket_key.data);
+	// 	gnutls_deinit(rq->tls_ctx);
+	// 	gnutls_certificate_free_credentials(rq->tls_creds);
+	// 	free(rq);
+	// 	return NULL;
+	// }
+
+	// gnutls_handshake_set_secret_function(rq->tls_ctx, secret_func);
+	// // gnutls_handshake_set_read_function(rq->tls_ctx, read_func);
+	// // gnutls_alert_set_read_function(rq->tls_ctx, alert_read_func);
+	// // gnutls_handshake_set_hook_function(rq->tls_ctx, GNUTLS_HANDSHAKE_CLIENT_HELLO,
+	// //                              GNUTLS_HOOK_POST, client_hello_cb);
+
+	return rq;
+}
+
+static int quic_recvfrom_recv(int fd, void *d)
+{
+	//ngtcp2_pkt_info pi;
+	uint32_t version;
+	ngtcp2_cid dcid, scid;
+	ngtcp2_pkt_hd hd;
+
+	/* Reset max lengths. */
+	struct quic_recvfrom *rq = (struct quic_recvfrom *)d;
+	rq->iov[RX].iov_len = KNOT_WIRE_MAX_PKTSIZE;
+	rq->msg[RX].msg_namelen = sizeof(struct sockaddr_storage);
+	rq->msg[RX].msg_controllen = sizeof(rq->pktinfo);
+
+	ssize_t nread = recvmsg(fd, &rq->msg[RX], MSG_DONTWAIT);
+	if (nread < 0) {
+		return 1;
+	}
+	rq->fd = fd;
+	rq->iov[RX].iov_len = nread;
+	//pi.ecn = msghdr_get_ecn(&rq->msg[RX], rq->msg[RX].msg_name);
+
+	int ret = 0;
+	switch (ret = ngtcp2_pkt_decode_version_cid(&version, &dcid.data, &dcid.datalen,
+                                                &scid.data, &scid.datalen, rq->buf[RX],
+                                                nread, QUIC_SV_SCIDLEN)) {
+	case 0:
+		break;
+	case NGTCP2_ERR_VERSION_NEGOTIATION:
+    //   send_version_negotiation(version, scid, scidlen, dcid, dcidlen, ep,
+    //                            *local_addr, &su.sa, msg.msg_namelen);
+    	return 0;
+    default:
+		return KNOT_NET_ECONNECT; //TODO maybe change return val
+	}
+
+	assert(dcid.datalen <= NGTCP2_MAX_CIDLEN);
+	knot_quic_conn_t *conn = knot_quic_table_find(rq->conns, &dcid);
+	if (conn == NULL) {
+		ret = ngtcp2_accept(&hd, rq->buf[RX], nread);
+		switch (ret) {
+		case 0:
+			break;
+		default:
+			return KNOT_NET_ECONNECT;
+		}
+
+		assert(hd.type == NGTCP2_PKT_INITIAL);
+		
+		// TODO validate addr
+
+		conn = knot_quic_table_add(rq->conns, &dcid);
+
+		ret = knot_quic_conn_recv(conn, rq->buf[RX], nread);
+		switch(ret) {
+
+		}
+		
+		size_t scid_num = ngtcp2_conn_get_num_scid(conn->conn);
+	}
+	//memcpy(conn.dcid, dcid, MIN(dcidlen, NGTCP2_MAX_CIDLEN));
+	//knot_quic_conn_hash(&conn);
+
+	return 0;
+}
+
+
+static udp_api_t quic_recvfrom_api = {
+	quic_recvfrom_init,
+	udp_recvfrom_deinit,
+	quic_recvfrom_recv,
+	udp_recvfrom_handle,
+	udp_recvfrom_send,
+};
+#endif /* LIBNGTCP2 */
+
+static bool is_quic_thread(const server_t *server, int thread_id)
+{
+	const unsigned size = server->handlers[IO_QUIC].size;
+	const unsigned *ids = server->handlers[IO_QUIC].handler.thread_id;
+	return size > 0 && thread_id >= ids[0] && thread_id <= ids[size - 1];
+}
+
+static int iface_udp_fd(const iface_t *iface, int thread_id, bool quic_thread,
+                        bool xdp_thread, void **xdp_socket)
 {
 	if (xdp_thread) {
 #ifdef ENABLE_XDP
@@ -413,6 +754,21 @@ static int iface_udp_fd(const iface_t *iface, int thread_id, bool xdp_thread,
 #else
 		assert(0);
 		return -1;
+#endif
+	} else if (quic_thread) { // QUIC thread.
+		if (iface->fd_quic_count == 0) { // No QUIC interfaces.
+			return -1;
+		}
+		if (thread_id < iface->quic_first_thread_id ||
+		    thread_id >= iface->quic_first_thread_id + iface->fd_quic_count) {
+			return -1; // Different QUIC interface.
+		}
+#ifdef ENABLE_REUSEPORT
+		size_t quic_wrk_id = thread_id - iface->quic_first_thread_id;
+		assert(quic_wrk_id < iface->fd_quic_count);
+		return iface->fd_quic[quic_wrk_id];
+#else
+		return iface->fd_quic[0];
 #endif
 	} else { // UDP thread.
 		if (iface->fd_udp_count == 0) { // No UDP interfaces.
@@ -436,10 +792,11 @@ static unsigned udp_set_ifaces(const server_t *server, size_t n_ifaces, fdset_t 
 	}
 
 	bool xdp_thread = is_xdp_thread(server, thread_id);
+	bool quic_thread = is_quic_thread(server, thread_id);
 	const iface_t *ifaces = server->ifaces;
 
 	for (const iface_t *i = ifaces; i != ifaces + n_ifaces; i++) {
-		int fd = iface_udp_fd(i, thread_id, xdp_thread, xdp_socket);
+		int fd = iface_udp_fd(i, thread_id, quic_thread, xdp_thread, xdp_socket);
 		if (fd < 0) {
 			continue;
 		}
@@ -478,6 +835,12 @@ int udp_master(dthread_t *thread)
 	if (is_xdp_thread(handler->server, thread_id)) {
 #ifdef ENABLE_XDP
 		api = &xdp_recvmmsg_api;
+#else
+		assert(0);
+#endif
+	} else if (is_quic_thread(handler->server, thread_id)) {
+#ifdef LIBNGTCP2
+		api = &quic_recvfrom_api;
 #else
 		assert(0);
 #endif
